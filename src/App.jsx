830ee649738c11c4dbbe89e73db686e75056c523
Goutham <gouthamshastry02@gmail.com>
2024-11-05 19:32:14 +0530

    Initial commit with navbar component added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <main className="bg-gray-900">
+      <div className="flex">
+        <h1 className="text-white">Goutham</h1>
+        <h1 className="text-red-500">J</h1>
+      </div>
+    </main>
+  );
+}
